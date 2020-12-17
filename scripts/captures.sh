743c75d77594ddc52ab280b163b8a93930d5e9af
estheeeeer <esthersolari@yahoo.com>
2020-12-17 07:18:28 +0100

    1ère nuit blanche 100%

--- scripts/captures.sh
+++ scripts/captures.sh
@@ -1,13 +1,162 @@
 #!/bin/bash
 
-usage=$(printf "Usage : ./captures.sh [OPTIONS]\neffectue des captures toutes les secondes\n[OPTIONS]\n-s,--set <directory> change le repertoire d'enregistrement des captures\n-d,--debug exécute et affiche toutes les logs\n-l,--log cat le fichier log.txt\n-g,--get prend une capture")
+usage=$(printf "USAGE : ./captures.sh [OPTIONS]\neffectue des captures toutes les secondes\n[OPTIONS]\n-s,--set <directory> change le repertoire d'enregistrement des captures\n-d,--debug exécute et affiche toutes les logs\n-l,--log cat le fichier log.txt\n-g,--get prend une capture\n-c,--clear supprime toutes les captures\n-h,--help display informations")
+error="\e[31merror :\e[39m"
+success="\e[32msuccess :\e[39m"
+phelp=": see --help for more informations\n"
 date=$(date '+%Y.%m.%d.%H.%M.%S')
 dated=$(date '+%H:%M:%S')
 nbc=59
-directory="/media/pi/NAS/captures/"
 debug=0
 
-if [ "$#" -gt "3" ]
+updatedir() {
+	directory=sed -i "1s/.*/"$(pwd)$1"/"log.txt
+}
+
+
+errorh() {
+	printf "$error $1 $phelp\n"
+	exit 1
+}
+
+
+errore() {
+       	printf "$error $1\n"
+	exit 1
+}
+
+
+sdir() {
+	read -p "$error le répertoire spécifié n'existe pas voulez-vous le créer ? O/n " create
+        while [ "$create" != "O" ] || [ "$create" != "o" ] || [ "$create" != "N" ] || [ "$create" != "n" ]
+	do
+		case $create in
+			O|o)
+                        	mkdir $create
+                        	if [ "$?" -eq 0 ]
+                        	then
+					updatedir $2
+                                	printf "$success répertoire créé et mis à jour\n"
+                                	exit 0
+                        	else
+                                	errore "une erreur est survenue"
+                        fi
+                        ;;
+                        N|n)
+				printf "Le répertoire n'a pas été mis à jour\n"
+                                ;;
+                        *)
+                                read -p "$error le répertoire spécifié n'existe pas voulez-vous le créer ? O/n" create
+                        ;;
+               	esac
+      	done
+}
+
+
+catlog() {
+	if [ -f log.txt ]
+        then
+		cat log.txt
+                exit 0
+        else
+            	printf "$usage\n"
+                exit 1
+	fi
+}
+
+
+cleard() {
+	directory=$(head -n 1 log.txt)
+	rm $directory/* && exit 
+}
+
+
+log() {
+	if [ ! -f log.txt ]
+        then
+		read -p "$error Veuillez définir un répertoire où enregistrer les captures : " directory
+                echo $directory>log.txt
+        else
+		directory=$(head -n 1 log.txt)
+        fi
+}
+
+
+logd() {
+	if [ ! -f log.txt ]
+        then
+               read -p "$error répertoire non défini le définir : " directory
+               echo $directory>log.txt
+        else
+               directory=$(head -n 1 log.txt)
+        fi
+}
+
+
+cam() {
+	if [ ! -e /dev/video0 ]
+        then
+		echo "$date -> la caméra n'est pas branchée">>log.txt
+                exit 1
+        fi
+}
+
+
+camd() {
+	if [ ! -e /dev/video0 ]
+        then
+        printf "$error $dated -> /dev/video0 n'existe pas\n"
+        exit 1
+        fi
+
+}
+
+
+dir() {
+	if [ ! -d $directory ]
+        then
+        	echo "$date -> le dossier $directory n'existe pas">>log.txt
+        	exit 1
+        fi
+}
+
+
+dird() {
+	if [ ! -d $directory ]
+        then
+                printf "$error $dated -> le dossier $directory n'existe pas\n"
+                exit 1
+        fi
+}
+
+
+fsw() {
+	fswebcam 2>/dev/null
+	a=$?
+	if [ "$(apt list 2>/dev/null|grep -o fswebcam)" != "fswebcam" ] || [ "$a" -eq "127" ]
+        then
+                printf "$error Vous n'avez pas fswebcam\n"
+		exit 1
+        fi
+}
+
+
+fswd() {
+	fswebcam 2>/dev/null
+	a=$?
+        if [ "$(apt list 2>/dev/null|grep -o fswebcam)" != "fswebcam" ]
+        then
+                printf "$error fswebcam non trouvé dans l'apt list\n"
+		exit 1
+	elif [ "a" -eq "127" ]
+	then
+		printf "$error commmand not found\n"
+		exit 1
+	fi
+}
+
+
+if [ "$#" -gt "2" ]
 then
 	printf "$usage\n"
 	exit 1
@@ -15,95 +164,69 @@ elif [[ "$#" -gt "1" ]]
 then
 	case  $1 in
 		-s|--set)
+			if [ "$#" -gt "2" ]
+			then
+				errorh "trop d'arguments"
+			else
+				if [ "$2" = "" ]
+                        	then
+                                	errorh "mauvais usage de l'option -s, --set"
+                        	fi
+
+                        	if [ -d $2 ]
+                        	then
+                                	updatedir $2&&printf "$success répertoire mis à jour\n"
+                        	else
+					sdir	
+					exit 0
+				fi
+			fi
 		;;
-		*) 
+		-g|--get) errorh "Mauvais usage de l'option -g,--get";;
+		-d|--debug) errorh "Mauvais usage de l'option -d,--debug";;
+		-l|--log) errorh "mauvais usage de l'option -l,--log";;
+		-c|--clear) errorh "mauvais usage de l'option -c,--clear";;
+		-h|--help)
 			printf "$usage\n"
-			exit 1
-		;;
+			exit 0;;
+		*)errorh "options inconnues";;
 	esac
-elif [ "$#" -ne "0" ]
+elif [ "$#" -eq "1" ]
 then
 	case $1 in 
-		-g|--get)
-			nbc=1
-		;;
-		-d|--debug)
-			debug=1
-		;;
-		-l|--log)
-			if [ -f log.txt ]
-			then 
-				cat log.txt
-				exit 0
-			else
-				echo "Le fichier log.txt n'existe pas"
-				exit 1
-			fi
-		;;
-		-s|--set)
-			if [ "$2" = "" ]
-			then
-				printf "$usage\n"
-				exit 1
-			fi
-			directory="$2"
-			if [ -d $directory ]
-			then
-				printf "Répertoire mis à jour\n"
-			else
-				printf "\e[31mLe répertoire spécifié n'existe pas\n"
-			fi
-		;;	
-		*)
+		-g|--get) nbc=1;;
+		-d|--debug) debug=1;;
+		-l|--log) catlog;;
+		-c|--clear) cleard;;
+		-h|--help)
 			printf "$usage\n"
-			exit 1
-		;;
+			exit 0;;
+		*)errorh "option inconnue";;
 	esac
 fi
 
 if [ "$debug" -eq "0" ]
 then
-	for i in {1..$nbc}
+	for i in $(seq 1 1 $nbc)
 	do
-		if [ ! -f log.txt ]
-		then
-			touch log.txt
-		fi
-	
-		if [ ! -e /dev/video0 ]
-		then
-			echo "$date -> la caméra n'est pas branchée">>log.txt
-			exit 1
-		fi
-	
-		if [ ! -d $directory ]
-		then
-			echo "$date -> le dossier $directory n'existe pas">>log.txt
-			exit 1
-		fi
-	
+		log
+		camd
+		dir
+		fsw
 		fswebcam -q $directory/$date.jpg&
 		sleep 1
 	
 	done
 else
-	for i in {1..$nbc}
+	for i in $(seq 1 1$nbc)
         do
-                if [ ! -e /dev/video0 ]
-                then
-                        printf "\e[31m$dated -> la caméra n'est pas branchée\n"
-                        exit 1
-                fi
-
-                if [ ! -d /media/pi/NAS/captures ]
-                then
-                        printf "\e[31m$dated -> le dossier $directory n'existe pas\n"
-			iebug=1
-                        exit 1
-                fi
-
+		printf "------------i = $i--------------\n"
+		logd&& printf "	directory = $directory\n"
+		camd&& printf "	/dev/video0 trouvé\n"
+		dird&& printf "	directory $directory existe\n"
+		fswd&& printf "	fswebcam installé\n"
                 fswebcam -q $directory/$date.jpg&
-		echo "$dated -> capture prise"
+		printf "$success $dated -> capture prise\n"
                 sleep 1
 	done
 fi
