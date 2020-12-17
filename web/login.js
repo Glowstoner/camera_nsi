6a222c886bee0532b445e8b66e15b106a1900d8f
Johann von Issou <glowstoner1@gmail.com>
2020-12-17 22:35:54 +0100

    added login validation

--- web/login.js
+++ web/login.js
@@ -1,10 +1,36 @@
-var error = $('#falsepasswd');
-
 $(document).ready(function (){
-    console.log("hello WORLD!");
-    error.remove();
+    console.log("Page chargée.");
+
+    $('#loginbutton').click(function (){
+        console.log("Connexion ...");
+        var password = $('#passwd').val();
+        console.log(password);
+
+        if(password) {
+            console.log("Mot de passe valide.");
+            connect(password);
+        }else {
+            error("Vous devez renseigner un mot de passe !");
+        }
+    });
 });
 
-function error() {
-    $('#logintitle').after(error)
+function connect(password) {
+    $.post("login.php", {passwd: password}, function(data, status) {
+        if(status == "success") {
+            var ret = JSON.parse(data);
+            if(ret.valid && ret.sucess) {
+                console.log("Connexion réussie !");
+            }else {
+                console.log("Connexion échouée !");
+                error("Mot de passe incorrect !");
+            }
+        }
+    });
+}
+
+function error(message) {
+    var content = "<div id=\"falsepasswd\"><span class=\"material-icons redcolor\" id=\"falsepasswdlogo\">error</span><p class=\"redcolor\" id=\"falsepasswdtext\">" + message + "</p></div>"
+    var target = document.getElementById("falsepasswd");
+    target.innerHTML = content;
 }
--- web/login.php
+++ web/login.php
@@ -1,3 +1,29 @@
 <?php
-    echo "test";
+$PASSWD = "test";
+
+function getReturnJSON($sucess, $valid) {
+    if(!$valid) {
+        $ret->valid = FALSE;
+        $ret->sucess = FALSE;
+        return json_encode($ret);
+    }
+
+    $ret->valid = TRUE;
+    if($sucess) {
+        $ret->sucess = TRUE;
+    }else {
+        $ret->sucess = FALSE;
+    }
+
+    return json_encode($ret);
+}
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    if(isset($_POST["passwd"])) {
+        echo getReturnJSON((htmlentities($_POST["passwd"]) == $PASSWD), TRUE);
+    }else {
+        $ret->valid = FALSE;
+        echo getReturnJSON(FALSE, FALSE);
+    }
+}
 ?>
